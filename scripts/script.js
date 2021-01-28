5f2964af7657fdfde05fb62629a36ec9a5bdca79
InLas <73998785+InLas@users.noreply.github.com>
2021-01-28 16:32:49 +0300

    Правки в index, залит catalog

--- scripts/script.js
+++ scripts/script.js
@@ -1,88 +1,73 @@
+$(document).ready(function(){
+  $('.catalog__slider').slick({
+      dots: true,
+      arrows: true,
+      draggable: false,
+      speed: 0,
+  });
+});
+
 let offset = 0;
-const sliderline1 = document.querySelector(".catalog__slider-line");
-const sliderline2 = document.querySelector(".services__slider__line");
-// let sliderbutton1 = document.querySelector(".slider__buttons-list__button--1");
-//   let sliderbutton2 = document.querySelector('.slider__buttons-list__button--2');
-//   let sliderbutton3 = document.querySelector('.slider__buttons-list__button--3');
-let MapOpen = document.querySelector(".about-us__map__open");
-let MapShow = document.querySelector(".about-us__modal__map");
-let MapClose = document.querySelector(".about-us__modal__close");
-let FormOpen = document.querySelector(".about-us__write-us__open");
-let FormShow = document.querySelector(".about-us__modal__write-us");
-let FormClose = document.querySelector(".about-us__modal__close--write-us");
-let UserName = document.querySelector(".modal__input");
-
-
-document.querySelector(".catalog__slider__next").addEventListener("click", function (evt)  {
-  evt.preventDefault()
-offset = offset + 620;
-if (offset > 620)  {
-    offset = 0
-}
-sliderline1.style.left = -offset + "px";
-});
-
-
-document.querySelector(".catalog__slider__prev").addEventListener("click", function (evt)  {
-  evt.preventDefault()
-offset = offset - 620;
-if (offset < 0)  {
-    offset = 620
-}
-sliderline1.style.left = -offset + "px";
-});
+const sliderLine = document.querySelector(".slider-box__line");
+let mapOpen = document.querySelector(".about-us__map-open");
+let mapShow = document.querySelector(".about-us__modal-map");
+let mapClose = document.querySelector(".about-us__modal-close");
+let formOpen = document.querySelector(".about-us__write-us-open");
+let formShow = document.querySelector(".about-us__modal-write-us");
+let formClose = document.querySelector(".modal-write-us__modal-close");
+let userName = document.querySelector(".modal-write-us__input");
 
 
 document.querySelector(".slider__buttons-list__button--1").addEventListener("click", function (evt)  {
-    evt.preventDefault()
+    evt.preventDefault();
   offset = offset + 0;
   if (offset > 0)  {
-      offset = 0
+      offset = 0;
   }
-  sliderline2.style.bottom = -offset + "px";
+  sliderLine.style.bottom = -offset + "px";
 });
 
 
 document.querySelector(".slider__buttons-list__button--2").addEventListener("click", function (evt)  {
-    evt.preventDefault()
+    evt.preventDefault();
   offset = offset + 285;
   if (offset > 0)  {
-      offset = 285
+      offset = 285;
   }
-  sliderline2.style.bottom = offset + "px";
+  sliderLine.style.bottom = offset + "px";
 });
 
 
 document.querySelector(".slider__buttons-list__button--3").addEventListener("click", function (evt)  {
-    evt.preventDefault()
+    evt.preventDefault();
   offset = offset + 570;
   if (offset > 0)  {
-      offset = 570
+      offset = 570;
   }
-  sliderline2.style.bottom = offset + "px";
+  sliderLine.style.bottom = offset + "px";
 });
 
   
-MapOpen.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  MapShow.classList.add("about-us__modal__map--show");
+mapOpen.addEventListener("click", function (evt) {
+  evt.preventDefault();;
+  mapShow.classList.add("about-us__modal-map--show");
 });
 
 
-MapClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  MapShow.classList.remove("about-us__modal__map--show");
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();;
+  mapShow.classList.remove("about-us__modal-map--show");
 });
 
 
-FormOpen.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  FormShow.classList.add("about-us__modal__write-us--show");
-  UserName.focus();
+formOpen.addEventListener("click", function (evt) {
+  evt.preventDefault();;
+  formShow.classList.add("about-us__modal-write-us--show");
+  userName.focus();
 });
 
 
-FormClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  FormShow.classList.remove("about-us__modal__write-us--show");
+formClose.addEventListener("click", function (evt) {
+  evt.preventDefault();;
+  formShow.classList.remove("about-us__modal-write-us--show");
 });
