84757d9c47d3e3fe6cd159117def4d05ed50a931
Сергей Сальников <73998785+InLas@users.noreply.github.com>
2021-05-28 20:09:58 +0300

    add fonts, refactoring, js

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,3 @@
-$(document).ready(function(){
-  $('.features__slider').slick({
-      dots: true,
-      arrows: true,
-      draggable: false,
-      speed: 0,
-  });
-});
-
 let offset = 0;
 const sliderLine = document.querySelector(".slider-box__line");
 let mapOpen = document.querySelector(".about-us__map-open");
@@ -16,9 +7,12 @@ let formOpen = document.querySelector(".about-us__write-us-open");
 let formShow = document.querySelector(".about-us__modal-write-us");
 let formClose = document.querySelector(".modal-write-us__modal-close");
 let userName = document.querySelector(".modal-write-us__input");
+let servicesButtons = document.querySelectorAll(".slider-buttons-list__button");
+let deliveryButton = document.querySelector(".slider-buttons-list__button--delivery");
+let warrantyButton = document.querySelector(".slider-buttons-list__button--warranty");
+let creditButton = document.querySelector(".slider-buttons-list__button--credit");
 
-
-document.querySelector(".slider-buttons-list__button--delivery").addEventListener("click", function (evt)  {
+deliveryButton.addEventListener("click", function (evt)  {
     evt.preventDefault();
   offset = offset + 0;
   if (offset > 0)  {
@@ -27,8 +21,7 @@ document.querySelector(".slider-buttons-list__button--delivery").addEventListene
   sliderLine.style.bottom = -offset + "px";
 });
 
-
-document.querySelector(".slider-buttons-list__button--warranty").addEventListener("click", function (evt)  {
+warrantyButton.addEventListener("click", function (evt)  {
     evt.preventDefault();
   offset = offset + 285;
   if (offset > 0)  {
@@ -37,8 +30,7 @@ document.querySelector(".slider-buttons-list__button--warranty").addEventListene
   sliderLine.style.bottom = offset + "px";
 });
 
-
-document.querySelector(".slider-buttons-list__button--credit").addEventListener("click", function (evt)  {
+creditButton.addEventListener("click", function (evt)  {
     evt.preventDefault();
   offset = offset + 570;
   if (offset > 0)  {
@@ -47,24 +39,37 @@ document.querySelector(".slider-buttons-list__button--credit").addEventListener(
   sliderLine.style.bottom = offset + "px";
 });
 
-  
+servicesButtons.forEach(item => {
+	item.addEventListener('click', function () {
+    let current = document.getElementsByClassName("slider-buttons-list__button--active");
+    current[0].className = current[0].className.replace(" slider-buttons-list__button--active", "");
+    this.className += " slider-buttons-list__button--active";
+	})
+});
+
+$(document).ready(function(){
+  $('.features__slider').slick({
+      dots: true,
+      arrows: true,
+      draggable: false,
+      speed: 0,
+  });
+});
+
 mapOpen.addEventListener("click", function (evt) {
   evt.preventDefault();
   mapShow.classList.add("about-us__modal-map--show");
 });
 
-
 mapClose.addEventListener("click", function () {
   mapShow.classList.remove("about-us__modal-map--show");
 });
-
 
 formOpen.addEventListener("click", function (evt) {
   evt.preventDefault();
   formShow.classList.add("about-us__modal-write-us--show");
   userName.focus();
 });
-
 
 formClose.addEventListener("click", function () {
   formShow.classList.remove("about-us__modal-write-us--show");
